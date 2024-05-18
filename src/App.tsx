e6a92d1750a431ab6cdc49ffb770dc2a01205249
Gabriel Resende <gabriel@sensio.com.br>
2024-05-18 12:59:13 -0300

    feat(App): set correct list filter

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,46 @@ interface Repo {
 
 function App() {
   const [repos, setRepos] = useState<Repo[]>([]);
+  // const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
+  const [filter, setFilter] = useState("");
+
+  console.log("App render");
 
   useEffect(() => {
     fetch("https://api.github.com/users/resendiK2/repos")
       .then((res) => res.json())
-      .then((repos) => setRepos(repos));
+      .then((repos) => {
+        setRepos(repos);
+        // setFilteredRepos(repos); // [2]
+      });
   }, []);
+
+  // useEffect(() => {
+  //   setFilteredRepos(
+  //     repos.filter((repo) =>
+  //       repo.name.toLowerCase().includes(filter.toLowerCase())
+  //     )
+  //   );
+  // }, [filter]);
+
+  const filteredRepos = filter?.length
+    ? repos.filter((repo) =>
+        repo.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : repos;
 
   return (
     <div>
-      <input name='search' type='text' placeholder='Buscar...' />
+      <input
+        name='search'
+        type='text'
+        placeholder='Buscar...'
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
 
       <ul>
-        {repos.map((repo) => (
+        {filteredRepos.map((repo) => (
           <li key={repo.name}>
             <h2>{repo.name}</h2>
           </li>
